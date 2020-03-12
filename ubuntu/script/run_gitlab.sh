7833d81b25858e45f3b9f7d950c6ce3a12e06758
kimsan0622 <kimsan0622@gmail.com>
2020-03-12 11:34:59 +0900

    Update run_gitlab.sh

--- ubuntu/script/run_gitlab.sh
+++ ubuntu/script/run_gitlab.sh
@@ -1,6 +1,10 @@
 export GITLAB_IP=10.1.92.5
 export GITLAB_DIR=/home/airc/gitlab
 
+mkdir ${GITLAB_DIR}/config
+mkdir ${GITLAB_DIR}/logs
+mkdir ${GITLAB_DIR}/data
+
 # gitlab/gitlab-ce:latest
 # gitlab/gitlab-ee:latest
 docker run --detach \
